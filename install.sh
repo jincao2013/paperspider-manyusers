f617a8ef45738e94e31a12926a0b1c4feede8b92
Jin Cao <caojin.phy@gmail.com>
2021-07-05 15:07:11 +0800

    fix install.sh uninstall.sh service.d

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
 
-sudo cp -rf paperspider-db /opt
+sudo cp -rf * /opt/paperspider
+sudo mv /opt/paperspider/paperspider-db /opt/paperspider-db
 
 sudo mkdir -f /etc/paperspider
 sudo cp config.json /etc/paperspider
 
 sudo cp service.d/paperspider.service /etc/systemd/system
 sudo systemctl enable paperspider.service
+sudo systemctl stop paperspider.service
 sudo systemctl start paperspider.service
 
--- uninstall.sh
+++ uninstall.sh
@@ -5,5 +5,6 @@ sudo systemctl disable paperspider.service
 sudo rm -f /etc/systemd/system/paperspider.service
 
 sudo rm -rf /opt/paperspider-db 
+sudo rm -rf /opt/paperspider
 
 sudo rm /etc/paperspider/config.json
