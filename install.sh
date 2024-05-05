4516ba79faf58154a309da8cb0bc2358efc64bea
jincao2013 <caojin.phy@gmail.com>
2024-05-05 11:34:45 +0800

    refine code

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
-sudo cp -rf * /opt/paperspider
-sudo mv /opt/paperspider/paperspider-db /opt/paperspider-db
+sudo mkdir -p /opt/paperspider
+sudo cp -rf * /opt/paperspider/
+sudo mv -rf /opt/paperspider/paperspider-db /opt/paperspider-db
 
-sudo mkdir -f /etc/paperspider
-sudo cp config.json /etc/paperspider
+sudo mkdir -p /etc/paperspider
+sudo cp config.json /etc/paperspider/
 
 sudo cp service.d/paperspider.service /etc/systemd/system
 sudo systemctl enable paperspider.service
 sudo systemctl stop paperspider.service
 sudo systemctl start paperspider.service
-
